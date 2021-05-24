f0ef7887bb93e314bbe2b2ca641d45f6fbb5a8a8
hendrik <hendriklop2106@hotmail.com>
2021-05-24 15:53:58 -0500

    routes added

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -19,12 +19,20 @@ import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +43,8 @@ import { DishService } from './services/dish.service';
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
